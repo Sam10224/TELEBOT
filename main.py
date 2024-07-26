063148f1cbc2852c6b4e8fb2a22a154c5666e8a7
Sam Jose <onungasfx@gmail.com>
2024-07-26 15:22:34 +0300

    update

--- main.py
+++ main.py
@@ -30,3 +30,83 @@ USER_AGENT: Final = "YourBotName/1.0 (https://yourwebsite.com/; your-email@examp
 wiki_wiki = wikipediaapi.Wikipedia(language="en", user_agent=USER_AGENT)
 openai.api_key = OPENAI_API_KEY
 
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("""
+—————————————————————
+┈╭━━━━━━━━━━━╮┈ ┈┃╭━━━╮┊╭━━━╮┃┈ ╭┫┃┈▇┈┃┊┃┈▇┈┃┣╮ ┃┃╰━━━╯┊╰━━━╯┃┃ ╰┫╭━╮╰━━━╯╭━╮┣╯ ┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈ ┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈ ┈╰━━━━━━━━━━━╯┈
+*/menu to display the menu*
+.……..… /´¯/)………....(\¯\.............
+……….../….//……….. …\\….\. ........
+………../….//………… ….\\….\. ......
+…../´¯/…./´¯\………../¯ \….\¯`\.....
+.././.../…./…./.|_……_|.\….\….\…\.\
+(.(….(….(…./.)..)..(..(.\….)….)….).) 
+.\…………….\/../…..\....\/…………../
+""")
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if "hello" in processed:
+        return "Hey there i am TELEBOT!"
+    if "how are you" in processed:
+        return "I've never been better!"
+    if "bot" in processed:
+        return ""
+    if "menu" in processed:
+        return f"""
+        ╭════〘 TELEBOT 〙═⊷⏣
+⬡│▸ /start
+⬡│▸ /menu
+⬡│▸ /pexels
+⬡│▸ /openai
+⬡│▸ /weather
+⬡│▸ /trivia
+⬡│▸ /youtube
+⬡│▸ /tiktok
+⬡│▸ /owner
+⬡│▸ /version
+⬡│▸ /translate
+⬡│▸ /morse
+⬡│▸ /time
+ └───────────────···▸
+The current date and time is\n {datetime.now()}
+        === [ V.1.0.0 ] ===
+"""
+    return ""
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f"[USER]: ({update.message.chat.id}) in {message_type}: {text}")
+
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return ""
+    else:
+        response: str =  handle_response(text)
+
+    print("[TELEBOT]:", response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"[TELEBOT]: update {update}\n caused error {context.error}\n")
+
+if __name__ == "__main__":
+    print("[TELEBOT]: Starting bot...")
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+
+    app.add_handler(CommandHandler(filters.TEXT, handle_message))
+
+    app.add_error_handler(error)
+
+    print("[TELEBOT]: All Systems Nominal!")
+    print("\n")
+    print("[TELEBOT]: Polling...")
+    app.run_polling(poll_interval=3)
