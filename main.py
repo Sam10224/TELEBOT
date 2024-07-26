1c4448476eb47a0764cf7b6a9fa3d4026b6340f3
Sam Jose <onungasfx@gmail.com>
2024-07-26 15:00:08 +0300

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from telegram import Bot, Update
 from telegram.error import BadRequest
 from googletrans import Translator, LANGUAGES
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from helper import dare, truth, insults, flirtLines, jokes
 
 # Replace with your own API keys and URLs if necessary
 TOKEN: Final = os.getenv("BOT_TOKEN")
@@ -23,3 +24,9 @@ PEXELS_API_KEY: Final = os.getenv("PEXELS_API_KEY")
 TRIVIA_API_KEY: Final = os.getenv("TRIVIA_API_KEY")
 YOUTUBE_API_KEY: Final = os.getenv("YOUTUBE_API_KEY")
 WEATHER_API_KEY: Final = os.getenv("WEATHER_API_KEY")
+
+WEATHER_URL: Final = "http://api.openweathermap.org/data/2.5/weather"
+USER_AGENT: Final = "YourBotName/1.0 (https://yourwebsite.com/; your-email@example.com)"
+wiki_wiki = wikipediaapi.Wikipedia(language="en", user_agent=USER_AGENT)
+openai.api_key = OPENAI_API_KEY
+
